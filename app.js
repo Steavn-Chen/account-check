e0b2bc1d70ed31f657f9a5eadf4e446b5f7a58fd
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-27 01:03:01 +0800

    新增 error、login 頁面，並新增 login get/post  兩條路由。

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
 const users = require('./user.json')
+const bodyParser =require('body-parser')
 
 const app = express()
 const port = 3000
@@ -9,6 +10,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -17,9 +19,21 @@ app.get('/', (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login");
 });
-app.get("/login", (req, res) => {
-  console.log(req.body,req.params,req.query)
-  res.render("login");
+app.post("/login", (req, res) => {
+  let message = ''
+  const { email, password } = req.body
+  console.log(email, password);
+  const checkAccount = users.find(i =>  i.email === email )
+  console.log(checkAccount);
+  if (!checkAccount) {
+    message = '此帳號不存在 !'
+    return res.render('error', { message })
+  } 
+  if (checkAccount.password !== password) {
+    message = "你的帳號或密碼錯誤 !";
+    return res.render("error", { message });
+  }
+  res.render("welcome", { user: checkAccount });
 });
 
 app.listen(port, () => {
