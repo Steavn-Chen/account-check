968f9f075b86a64117a773f5e898d17948c69ac2
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-26 13:38:34 +0800

    載入 user.json 檔案，並設置靜態檔案資料 public 且載入

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
+const users = require('./user.json')
 
 const app = express()
 const port = 3000
@@ -7,10 +8,15 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 app.listen(port, () => {
   console.log(`The account-check WEB running on http://localhost:${port}`)
