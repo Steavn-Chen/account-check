d42d67aa92ebef140c6e969712c96c3b39d05835
Steavn Chen <tom9876555@yahoo.com.tw>
2022-03-26 13:25:20 +0800

    安裝 express-handlebars 並載入，創建 .gitignore 檔案編寫不要上傳到github的檔案類型

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index')
 })
 
 app.listen(port, () => {
